3d67f3fec21e4f795fefc31def76a1db0419ef44
java2888 <2975370834@qq.com>
2017-09-07 01:57:21 +0800

    update.

--- JavaNIO/src/nio/SimpleBlockingServer.java
+++ JavaNIO/src/nio/SimpleBlockingServer.java
@@ -1,22 +1,38 @@
 package nio;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SimpleBlockingServer {
-	private static final int port=8080;
-	
-	private static void handle(Socket socket){
-		
+	private static final int port = 8080;
+
+	private static void handle(Socket socket) {
+
+		try (
+				InputStream inputStream = socket.getInputStream(); 
+				OutputStream outputStream = socket.getOutputStream();
+		) 
+		{
+			int data;
+			while( (data=inputStream.read())!=-1 ){
+				outputStream.write(data);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		 ServerSocket serverSocket=new ServerSocket(port);
-		 while(true){
-			 Socket socket=serverSocket.accept();
-			 handle(socket);
-		 }
+		ServerSocket serverSocket = new ServerSocket(port);
+		while (true) {
+			Socket socket = serverSocket.accept();
+			handle(socket);
+		}
 	}
 
 }
